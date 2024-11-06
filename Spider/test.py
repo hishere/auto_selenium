c714e0149d0250303752f3299d04a8d1079694e6
hishere <1971902975@qq.com>
2024-11-06 14:19:10 +0800

    jdj

--- Spider/test.py
+++ Spider/test.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 import os
+import time
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
@@ -12,6 +13,10 @@ chromedriver = "/usr/bin/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
 driver.get("https://bsy-cdn-zhgame.yunyungquan.com/pengpengqiu/index.html?openid=opua_jrbKwEGa4xESBuQMqUnOhUw&activeid=232&_t=12")
-ActionChains(driver).move_by_offset(200, 100).click().perform() 
+#ActionChains(driver).move_by_offset(200, 100).click().perform() 
 #print(driver.title)
+x = driver.get_screenshot_as_base64()
+image = base64.b64decode(x)
+print(image)
+time.sleep(20)
 driver.quit()
