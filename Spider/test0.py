a1c97a7bd152599cfec743218e2074665baa74c0
Nooneb <56129009+hishere@users.noreply.github.com>
2025-03-12 12:56:15 +0800

    更新 test0.py

--- Spider/test0.py
+++ Spider/test0.py
@@ -5,15 +5,16 @@ import os
 import time
 import base64
 import io
+from webdriver_manager.chrome import ChromeDriverManager
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
-chromedriver = "/usr/bin/chromedriver"
-os.environ["webdriver.chrome.driver"] = chromedriver
-driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
+#chromedriver = "/usr/bin/chromedriver"
+#os.environ["webdriver.chrome.driver"] = chromedriver
+driver = webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options)
 driver.get("https://bsy-cdn-zhgame.yunyungquan.com/pengpengqiu/index.html?openid=opua_jrbKwEGa4xESBuQMqUnOhUw&activeid=232&_t=12")
 time.sleep(10)
 ActionChains(driver).move_by_offset(400, 270).click().perform()#开始
