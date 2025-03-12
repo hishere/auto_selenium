68e6364adbc0d2918886213c618294ce82947028
Nooneb <56129009+hishere@users.noreply.github.com>
2025-03-12 12:32:17 +0800

    更新 Spider

---
+++ Spider/test.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+import os
+import time
+import base64
+import io
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chromedriver = "/usr/bin/chromedriver"
+os.environ["webdriver.chrome.driver"] = chromedriver
+driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
+driver.get("https://hishere.github.io")
+print(driver.title)
